06296f9c5c43a5b5ff7ce6189a033cba28d1a33c
HubanHubanov <huban.hubanov@gmail.com>
2024-04-27 05:53:41 +0300

    fix create movie form

--- src/config/configHandlebars.js
+++ src/config/configHandlebars.js
@@ -5,8 +5,8 @@ function configHandlebars(app) {
     app.engine("hbs", handlebars.engine({
         extname: "hbs"
     }));
-    app.set("view engine", hbs);
-    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "hbs");
+    app.set("views", path.resolve("src/views"));
 
     return app;
 }
