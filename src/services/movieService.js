c523af88e799d557702d0591f2eb4a92d1eafb6c
HubanHubanov <huban.hubanov@gmail.com>
2024-04-30 11:32:49 +0300

    show dynamic movies on home page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,16 @@
-const movies = [];
+const movies = [{
+    title: 'Jungle cruise',
+    genre: 'adventure',
+    director: 'Stallone',
+    date: '2222',
+    imageUrl: '/img/the-little-mermaid.jpg',
+    rating: '8.1',
+    description: 'Nice movie'
+  }];
+
+  exports.getAll = () => {
+      return movies.slice();
+  }
 
 exports.create = (movieData) => {
     movies.push(movieData);
